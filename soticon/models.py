ac661c24970c81ba136d3a034f961f8a84164ccc
Lucas Soares <soareslukas9090@gmail.com>
2024-04-11 08:52:47 -0300

    1º migrations soticon

--- soticon/models.py
+++ soticon/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
-# Create your models here.
+from gerUsuarios import models as modelsUsusarios
+
+# Pensar em Signals como gatilhos se precisar
+
+
+class UserSoticon(modelsUsusarios.Base):
+    usuario = models.OneToOneField(
+        modelsUsusarios.User,
+        related_name="user_soticon",
+        on_delete=models.CASCADE,
+        null=False,
+    )
+    faltas = models.IntegerField(null=False)
+
+
+class Strike(modelsUsusarios.Base):
+    user_soticon = models.ForeignKey(
+        UserSoticon,
+        related_name="strike_user_soticon",
+        on_delete=models.CASCADE,
+        null=False,
+    )
+    data = models.DateField(auto_now_add=True, null=False)
+
+
+class Justificativa(modelsUsusarios.Base):
+    strike = models.ForeignKey(
+        Strike,
+        related_name="justificativa_strike",
+        on_delete=models.CASCADE,
+        null=False,
+    )
+    data = models.DateField(auto_now_add=True, null=False)
+    obs = models.CharField(max_length=512, null=False)
+
+
+class PosicaoFila(modelsUsusarios.Base):
+    num_ticket = models.IntegerField(null=False, unique=True)
+
+
+class Rota(modelsUsusarios.Base):
+    obs = models.CharField(max_length=512)
+    data = models.DateField(null=False)
+    status = models.CharField(max_length=30, null=False)
+    horario = models.DateTimeField(null=False)
+
+
+class Tickets(models.Model):
+    rota = models.ForeignKey(
+        Rota, related_name="tickets_rota", on_delete=models.RESTRICT, null=False
+    )
+    user_soticon = models.ForeignKey(
+        UserSoticon,
+        related_name="tickets_soticon",
+        on_delete=models.CASCADE,
+        null=False,
+    )
+    usado = models.BooleanField(default=False, null=False)
+    reservado = models.BooleanField(default=False, null=False)
+    posicao_fila = models.ForeignKey(
+        PosicaoFila,
+        related_name="tickets_posicao",
+        on_delete=models.RESTRICT,
+        null=False,
+    )
+
+
+class Regras(modelsUsusarios.Base):
+    descricao = models.CharField(max_length=512, null=False)
+    parametro = models.IntegerField(null=False)
