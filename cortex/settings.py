07063490073fdb7810360f3635713bb0b50386c3
DevIthalo <ithalocarvalho2@gmail.com>
2024-04-16 14:13:19 -0300

    UPDATE: ADD HOST FRONT

--- cortex/settings.py
+++ cortex/settings.py
@@ -38,10 +38,11 @@ CSRF_TRUSTED_ORIGINS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:8000",
+    "http://192.168.18.69:5500"
 ]
 
 CORS_ALLOW_ALL_ORIGINS: False
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://192.168.18.69:5500"]
 CORS_ALLOW_METHODS = ["*"]
 
 AUTH_USER_MODEL = "gerUsuarios.User"
