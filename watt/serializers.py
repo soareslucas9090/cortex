fb81575677c52df8d86e502ba71d10d7fe287e43
Lucas Soares <soareslukas9090@hotmail.com>
2024-07-05 00:20:14 -0300

    feat: calculo de consumo

--- watt/serializers.py
+++ watt/serializers.py
@@ -6,8 +6,9 @@ from .models import *
 class LeituraSerializer(serializers.ModelSerializer):
     class Meta:
         model = Leitura
-        fields = [
-            "corrente",
-            "voltagem",
-            "potencia",
-        ]
+        fields = ["corrente", "voltagem", "potencia", "valor_consumo"]
+
+    valor_consumo = serializers.SerializerMethodField(read_only=True)
+
+    def get_valor_consumo(self, obj):
+        return 0.853 * (obj.potencia / 1000)
