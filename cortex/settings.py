7a6f5128f842b29670ff4c8d7bc4b16282b98de4
Lucas Soares <soareslukas9090@hotmail.com>
2024-09-03 14:43:51 -0300

    fix: definição do static_root

--- cortex/settings.py
+++ cortex/settings.py
@@ -153,8 +153,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = "static/"
 
-STATIC_URL = "/static/"
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
